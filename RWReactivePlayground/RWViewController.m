63efe773a84af7db0346d166d3fd140f26e1bc4a
THANHLINH <thanhlinh9191@iMac-THANHLINH.local>
2015-09-05 18:23:16 +0700

    B7 : ADD SIDE-EFFECT

--- RWReactivePlayground/RWViewController.m
+++ RWReactivePlayground/RWViewController.m
@@ -98,12 +98,31 @@
 //         NSLog(@"Sign in result: %@", x);
 //     }];
     //full pipeline
-    [[[self.signInButton
-       rac_signalForControlEvents:UIControlEventTouchUpInside]
+//    [[[self.signInButton
+//       rac_signalForControlEvents:UIControlEventTouchUpInside]
+//      flattenMap:^id(id x) {
+//          return [self signInSignal];
+//      }]
+//     subscribeNext:^(NSNumber *signedIn) {
+//         BOOL success = [signedIn boolValue];
+//         self.signInFailureText.hidden = success;
+//         if (success) {
+//             [self performSegueWithIdentifier:@"signInSuccess" sender:self];
+//         }
+//     }];
+    
+    //B7: ADD SIDE EFFECT
+    [[[[self.signInButton
+        rac_signalForControlEvents:UIControlEventTouchUpInside]
+       doNext:^(id x) {
+           self.signInButton.enabled = NO;
+           self.signInFailureText.hidden = YES;
+       }]
       flattenMap:^id(id x) {
           return [self signInSignal];
       }]
      subscribeNext:^(NSNumber *signedIn) {
+         self.signInButton.enabled = YES;
          BOOL success = [signedIn boolValue];
          self.signInFailureText.hidden = success;
          if (success) {
